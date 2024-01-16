9808c54de408367fab52f6d70252e2977a0d9109
Jordan Harris <jordanharris1175@gmail.com>
2024-01-16 16:25:45 -0500

    Add basic structure of contact with Google Maps
    Clean up the tab logic in index.js as well

--- src/contact-page.js
+++ src/contact-page.js
@@ -1,7 +1,38 @@
 function contactComponent() {
   const element = document.createElement('div');
   element.id = 'contact';
+
+  const title = document.createElement('h1');
+  const phoneNumber = document.createElement('div');
+  const address = document.createElement('div');
+  const map = createMap();
+
+  title.id = 'contact-title';
+  title.textContent = 'Contact Us';
+
+  phoneNumber.id = 'phone-number';
+  phoneNumber.textContent = '123-456-7890';
+
+  address.id = 'address';
+  address.textContent = '123 Main Street, Anytown, USA';
+  // address.textContent = '25 Ellis St NE, Atlanta, GA 30303';
+
+  element.appendChild(title);
+  element.appendChild(phoneNumber);
+  element.appendChild(address);
+  element.appendChild(map);
   return element;
+}
+
+function createMap() {
+  const map = document.createElement('iframe');
+  map.id = 'google-maps';
+  map.src =
+    'https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d986.1768746531768!2d-84.38691093327049!3d33.75832989997066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d33.75864679935642!2d-84.38625385468929!5e0!3m2!1sen!2sus!4v1705439135177!5m2!1sen!2sus';
+  map.allowFullscreen = '';
+  map.loading = 'lazy';
+  map.referrerpolicy = 'no-referrer-when-downgrade';
+  return map;
 }
 
 export default contactComponent;
--- src/index.js
+++ src/index.js
@@ -5,35 +5,34 @@ import contactPage from './contact-page';
 
 const content = document.getElementById('content');
 
-function tabListeners() {
+function createTabLogic() {
   const tabs = document.querySelectorAll('.tablinks');
   const main = mainPage();
   const menu = menuPage();
   const contact = contactPage();
 
-  tabs.forEach((tab) => {
-    tab.addEventListener('click', (ev) => {
+  for (const tab of tabs) {
+    tab.addEventListener('click', () => {
       // remove active from all tab buttons
-      tabs.forEach((tab) => {
-        tab.classList.remove('active');
-      });
-      // Then set active for the correct tab and replace content
-      if (ev.target.id === 'main-tab') {
-        tab.classList.add('active');
+      tabs.forEach((tab) => tab.classList.remove('active'));
+      // then add active to current tab
+      tab.classList.add('active');
+
+      // replace content with corresponding tab
+      if (tab.id === 'main-tab') {
         content.replaceChildren(main);
-      } else if (ev.target.id === 'menu-tab') {
-        tab.classList.add('active');
+      } else if (tab.id === 'menu-tab') {
         content.replaceChildren(menu);
-      } else if (ev.target.id === 'contact-tab') {
-        tab.classList.add('active');
+      } else if (tab.id === 'contact-tab') {
         content.replaceChildren(contact);
       }
     });
-  });
+  }
+  // tabs.forEach((tab) => {});
 
-  // Load main by default
+  // Click main by default
   document.getElementById('main-tab').click();
 }
 
-tabListeners();
+createTabLogic();
 document.body.appendChild(content);
