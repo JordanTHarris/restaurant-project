393cadc275be1439a0ffb4e1da4f7df1941ed25c
Jordan Harris <jordanharris1175@gmail.com>
2024-01-17 15:11:51 -0500

    Change styling to use background-image in css
    and add a tab bar so the top is still colored

--- src/main-page.js
+++ src/main-page.js
@@ -1,16 +1,10 @@
-import MainImage from './images/main-page-image.jpg';
-
 function mainComponent() {
   const element = document.createElement('div');
-  const image = new Image();
   const textContainer = document.createElement('div');
   const h1 = document.createElement('h1');
   const description = document.createElement('div');
 
   element.id = 'main';
-
-  image.src = MainImage;
-  image.id = 'main-image';
 
   textContainer.id = 'main-text-container';
 
@@ -20,11 +14,8 @@ function mainComponent() {
   description.textContent = 'Come join us for a quality meal and a friendly atmosphere';
   description.id = 'main-description';
 
-  element.appendChild(image);
   textContainer.appendChild(h1);
   textContainer.appendChild(description);
-  // element.appendChild(h1);
-  // element.appendChild(description);
   element.appendChild(textContainer);
   return element;
 }
